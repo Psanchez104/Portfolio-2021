f3153a067820d0c2c0e745ff07216d804cbb2958
Pablo Sanchez <46680879+Psanchez104@users.noreply.github.com>
2020-11-18 18:08:19 -0700

    Add files via upload

--- form-submission.php
+++ form-submission.php
@@ -1,11 +1,18 @@
 <?php
+// ini_set('display_errors', '1');
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $message = $_REQUEST['message'];
 
-// if(isset($name) && isset($email) && isset($message)){
-    mail("PabloE94Sanchez@gmail.com", "Form to email message", $message, "From: $email");
-// }
-
-// echo($name);
+$to = "form-submit@pablosanchezdeveloper.com";
+$subject = "Contact form Submission";
+//message already created above
+$header = "From: $email";
+    $a = mail($to, $subject, $message, $header);
+    if ($a){
+        print("Message was sent!");
+    } else {
+        print("Message wasn't sent, please check that you have changed emails in the bottom");
+    }
 ?>
+
